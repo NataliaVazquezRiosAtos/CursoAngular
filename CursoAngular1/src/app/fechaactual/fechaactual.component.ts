28045b612ba6978686bad88b60d4596fd3089107
natalia vazquez <mi correo>
2018-07-19 02:26:43 +0200

    video 8 : componentes anidados

--- CursoAngular1/src/app/fechaactual/fechaactual.component.ts
+++ CursoAngular1/src/app/fechaactual/fechaactual.component.ts
@@ -11,9 +11,31 @@ import { Component } from '@angular/core';
 /* 3. styleUrls :    nos dice la ruta de los estilos propios del componente ( csss ) */ 
 
 @Component({
+
   selector: 'app-fechaactual',
-  templateUrl: './fechaactual.component.html',
+
+  // templateUrl: './fechaactual.component.html',
+  // si ponemos 'template' en vez de 'templateUrl' se puede insertar el codigo html dentro de ``
+  // si borrara el archivo de la plantilla 'fechaactual.component.html' y funciona igual
+
+  template:`
+  
+    <div class=" caja cajaFechaActual text-center"> 
+
+      <h1 class="text-danger">COMPONENTES ANIDADOS : FECHAACTUAL + COPYRIGHT </h1>
+        
+      <!-- {{ hoy }}: renderiza el valor que tiene la variable en el  componente  -->     
+      <!-- date: 'd/M/y H:h' : formatea la fecha :  dia , mes , año , hora y minuto  --> 
+      <h2 class="text-success">Fecha actual : {{ hoy | date: 'd/M/y H:h'}}</h2>
+     
+      <app-copyright></app-copyright>  
+        
+    </div> 
+  
+  `,
+
   styleUrls: ['./fechaactual.component.css']
+
 })
 
 /* para que pueda ser usado el componente , se crea la clase y se exporta */ 
