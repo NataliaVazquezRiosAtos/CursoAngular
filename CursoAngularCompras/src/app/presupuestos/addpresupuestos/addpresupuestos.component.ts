d32dd7d6e9db34ba95c197fc2fb8cdec52dcb072
Vazquez Rios <natalia.vazquezrios.external@atos.net>
2018-07-23 14:03:58 +0200

    video 43 : formulario reactive II

--- CursoAngularCompras/src/app/presupuestos/addpresupuestos/addpresupuestos.component.ts
+++ CursoAngularCompras/src/app/presupuestos/addpresupuestos/addpresupuestos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup , FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-addpresupuestos',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpresupuestosComponent implements OnInit {
 
-  constructor() { }
+  // ATRIBUTOS
+
+  // objeto en el que se rellena con los datos del presupuesto enviados a traves del formulario
+  presupuestoForm: FormGroup;
+
+  presupuesto: any;
+
+ 
+  constructor( private pf: FormBuilder) { }
 
   ngOnInit() {
+
+    // 'group()' metodo para almacenar/asociar los diferentes campos
+    this.presupuestoForm= this.pf.group({
+      proveedor: '',
+      fecha: '',
+      concepto: '',
+      baseimponible: '',
+      tipoiva: '',
+      total: '',
+    });
+
+  }
+
+  // metodo añarir presupuestp desde  formulario
+  enviarFormularioPresupuesto(){
+     this.presupuesto = this.savePresupuesto();
+  }
+
+  savePresupuesto(){
+
+    const savePresupuesto = {
+
+      proveedor: this.presupuestoForm.get('proveedor').value,
+      fecha: this.presupuestoForm.get('fecha').value,
+      concepto: this.presupuestoForm.get('concepto').value,
+      baseimponible: this.presupuestoForm.get('baseimponible').value,
+      tipoiva: this.presupuestoForm.get('tipoiva').value,
+      total: this.presupuestoForm.get('total').value,     
+
+    }
+
+    return savePresupuesto;
+
   }
 
 }
