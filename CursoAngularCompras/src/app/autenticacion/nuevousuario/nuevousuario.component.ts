ddbecb328af14bd1b99ea1adb6295c699a10ad7b
Vazquez Rios <natalia.vazquezrios.external@atos.net>
2018-07-28 01:35:33 +0200

    video 53 : registro II

--- CursoAngularCompras/src/app/autenticacion/nuevousuario/nuevousuario.component.ts
+++ CursoAngularCompras/src/app/autenticacion/nuevousuario/nuevousuario.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+/*********************************************************************************************************/
+/****************************************  IMPORTANCIONES ANGULAR ****************************************/
+/*********************************************************************************************************/
+
+
+import { Component , OnInit } from '@angular/core';
+
+// importaciones para formularios
+import { FormControl , FormGroup , FormBuilder , Validator , Validators } from '@angular/forms';
+
+
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+/*********************************************************************************************************/
+
 
 @Component({
   selector: 'app-nuevousuario',
   templateUrl: './nuevousuario.component.html',
   styleUrls: ['./nuevousuario.component.css']
 })
+
 export class NuevousuarioComponent implements OnInit {
 
-  constructor() { }
+   // ATRIBUTOS
 
-  ngOnInit() {
+   // objeto formulario
+   registroForm: FormGroup;
+
+   // objeto usuario
+   userdata: any; 
+ 
+   constructor( private formBuilder : FormBuilder ) { } 
+   
+   ngOnInit() {
+
+    this.registroForm = this.formBuilder.group({
+
+      'nombre' : [ '' , [ Validators.required , Validators.minLength(2) , Validators.maxLength(20) ] ] ,
+
+      'email' : [ '' , [ Validators.required , Validators.email ] ] ,
+
+      'password' : [ '' , [Validators.required , Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$' ) , Validators.minLength(8) ] ]
+     
+    })
+ 
   }
+ 
+  // para enviar los datos del formulario a bbdd
+  onSubmit() {
 
+    this.userdata = this.saveUserdata();
+
+  }
+ 
+  // para guardar el usuario en bbdd
+  saveUserdata() {
+
+    const saveUserdata = {
+
+      nombre: this.registroForm.get('nombre').value,
+
+      email: this.registroForm.get('email').value,
+
+      password: this.registroForm.get('password').value
+
+    }
+ 
+     return saveUserdata;
+
+  }
+ 
 }
